d7f70656e430b9408e4898808159d156ab853148
eacdy0000@126.com <>
2017-11-20 16:23:36 +0800

    代码fix

--- zipkin-server/src/main/java/com/itmuch/yes/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/itmuch/yes/ZipkinServerApplication.java
@@ -4,13 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
-import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
 @EnableZipkinStreamServer
 @EnableDiscoveryClient
 public class ZipkinServerApplication {
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         SpringApplication.run(ZipkinServerApplication.class, args);
     }
 }
