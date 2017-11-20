9f5b8684016c4aa4eb4789d4fa7826bd2088fe70
eacdy0000@126.com <>
2017-11-20 13:47:30 +0800

    turbine

---
+++ turbine-stream-server/src/main/java/com/migu/tsg/mircoservice/tubine/TurbineStreamServerApplication.java
@@ -0,0 +1,16 @@
+package com.migu.tsg.mircoservice.tubine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.cloud.netflix.turbine.stream.TurbineApplication;
+
+@SpringBootApplication
+@EnableTurbineStream
+@EnableDiscoveryClient
+public class TurbineStreamServerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TurbineApplication.class, args);
+	}
+}
