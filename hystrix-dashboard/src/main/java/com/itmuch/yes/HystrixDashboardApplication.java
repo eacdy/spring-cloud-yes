cea53bbcc1e1003cc38566132a385c4d1fb517dd
eacdy0000@126.com <>
2017-11-20 20:25:16 +0800

    fix

--- hystrix-dashboard/src/main/java/com/itmuch/yes/HystrixDashboardApplication.java
+++ hystrix-dashboard/src/main/java/com/itmuch/yes/HystrixDashboardApplication.java
@@ -4,10 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
-@Controller
 @EnableHystrixDashboard
 @EnableDiscoveryClient
 public class HystrixDashboardApplication {
