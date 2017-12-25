146f272945b4c5d14bbb5338d85802613c785212
eacdy0000@126.com <>
2017-12-25 16:19:58 +0800

    zipkin 使用mq方式 收集数据

--- zipkin-server/src/main/java/com/itmuch/yes/ZipkinServerApplication.java
+++ zipkin-server/src/main/java/com/itmuch/yes/ZipkinServerApplication.java
@@ -3,10 +3,10 @@ package com.itmuch.yes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import zipkin.server.EnableZipkinServer;
 
 @SpringBootApplication
-@EnableZipkinStreamServer
+@EnableZipkinServer
 @EnableDiscoveryClient
 public class ZipkinServerApplication {
     public static void main(String[] args) {
