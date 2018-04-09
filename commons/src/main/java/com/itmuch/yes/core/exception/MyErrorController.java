118eaaa0b2e7c718f25a5884f86efc36ab60155f
eacdy0000@126.com <>
2018-04-09 17:31:54 +0800

    bug fix.

--- commons/src/main/java/com/itmuch/yes/core/exception/MyErrorController.java
+++ commons/src/main/java/com/itmuch/yes/core/exception/MyErrorController.java
@@ -1,9 +1,10 @@
 package com.itmuch.yes.core.exception;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.itmuch.yes.core.convert.AjaxResult;
+import com.google.common.collect.Maps;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.web.BasicErrorController;
+import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.http.HttpStatus;
@@ -12,11 +13,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.RequestAttributes;
+import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Map;
 
 /**
@@ -24,7 +26,10 @@ import java.util.Map;
  */
 @Controller
 @RequestMapping("/error")
+@Slf4j
 public class MyErrorController extends BasicErrorController {
+
+    public static final String ERROR_ATTRIBUTE = DefaultErrorAttributes.class.getName() + ".ERROR";
 
     @Autowired
     public MyErrorController(ErrorAttributes errorAttributes, ServerProperties serverProperties) {
@@ -46,31 +51,46 @@ public class MyErrorController extends BasicErrorController {
         Map<String, Object> body = super.getErrorAttributes(request,
                 isIncludeStackTrace(request, MediaType.ALL));
 
+        log.debug("原始消息体：{}", body);
+
         String message = body.get("message") != null ? (String) body.get("message") : null;
+        String error = body.get("error") != null ? (String) body.get("error") : null;
+
         Integer statusCode = body.get("status") != null ? (Integer) body.get("status") : null;
 
-        Object exception = body.get("exception");
-        if (exception != null && exception instanceof BizRuntimeException) {
+        Throwable exception = this.getException(request);
+
+        // 如果是业务异常，就进行处理，使用自己定义的错误码
+        if (exception instanceof BizRuntimeException) {
             BizRuntimeException bre = (BizRuntimeException) exception;
             statusCode = bre.getCode();
         }
 
-        AjaxResult<Object> build = AjaxResult.builder()
-                .status(statusCode)
-                .error(message)
-                .message(message)
-                .data(body)
-                .build();
-        Map res = null;
-        try {
-            ObjectMapper mapper = new ObjectMapper();
-            String s = mapper.writeValueAsString(build);
-            res = mapper.readValue(s, Map.class);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        Map<String, Object> res = Maps.newHashMap();
+        res.put("status", statusCode);
+        res.put("error", error);
+        res.put("message", message);
+        res.put("data", body);
 
         HttpStatus status = super.getStatus(request);
-        return new ResponseEntity<Map<String, Object>>(res, status);
+        return new ResponseEntity<>(res, status);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> T getAttribute(RequestAttributes requestAttributes, String name) {
+        return (T) requestAttributes.getAttribute(name, RequestAttributes.SCOPE_REQUEST);
+    }
+
+    private Throwable getException(HttpServletRequest request) {
+        RequestAttributes requestAttributes = new ServletRequestAttributes(request);
+        Throwable exception = getAttribute(
+                requestAttributes,
+                ERROR_ATTRIBUTE
+        );
+
+        if (exception == null) {
+            exception = getAttribute(requestAttributes, "javax.servlet.error.exception");
+        }
+        return exception;
     }
 }
