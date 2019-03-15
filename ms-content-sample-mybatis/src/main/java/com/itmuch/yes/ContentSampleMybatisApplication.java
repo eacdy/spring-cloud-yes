f7bfa36e478681aa9ed2aa3b06b1ff72bbcb0fcf
eacdy0000@126.com <>
2019-03-15 21:08:34 +0800

    FIX Turbine出错

--- ms-content-sample-mybatis/src/main/java/com/itmuch/yes/ContentSampleMybatisApplication.java
+++ ms-content-sample-mybatis/src/main/java/com/itmuch/yes/ContentSampleMybatisApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import tk.mybatis.spring.annotation.MapperScan;
 
+/**
+ * @author itmuch.com
+ */
 @SpringBootApplication
 @MapperScan("com.itmuch.yes.repository")
 public class ContentSampleMybatisApplication {
